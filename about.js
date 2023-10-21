2516bdc5d8d3666d99b6a11f28dd0737304b903d
Archico <104429357+archicos@users.noreply.github.com>
2023-10-22 00:59:07 +0700

    Add files via upload

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
-let circularProgress = document.querySelector(".circular-progress"),
-    progressValue = document.querySelector(".progress-value");
+// 
+let circularProgress = document.querySelector(".circular-progress .leadership"),
+    progressValue = document.querySelector(".progress-value .leadership");
 
 let progressStartValue = 0,
     progressEndValue = 90,
@@ -16,3 +17,24 @@ let progress = setInterval(() => {
     }
     console.log(progressStartValue)
 }, speed)
+
+// 
+let circularProgress2 = document.querySelector(".circular-progress .public-speaking"),
+    progressValue2 = document.querySelector(".progress-value .public-speaking");
+
+let progressStartValue2 = 0,
+    progressEndValue2 = 90,
+    speed2 = 15;
+
+let progress2 = setInterval(() => {
+    progressStartValue2++;
+
+    progressValue2.textContent = `${progressStartValue2}%`
+    circularProgress2.style.background = `conic-gradient(rgb(0, 90, 90) ${progressStartValue2 * 3.6}deg, #ededed 0deg)`
+
+    if(progressStartValue2 == progressEndValue2) {
+        clearInterval(progress2);
+    }
+    console.log(progressStartValue2)
+}, speed2)
+
--- portfolio.js
+++ portfolio.js
@@ -1,24 +1,22 @@
 'use strict';
 
-'use strict';
-
 // TECHNOLOGY
 
 // Clicknic
 
-const windowBlog1 = document.querySelector("#window-blog-1");
+const windowTech1 = document.querySelector("#window-project-tech-1");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal1 = document.querySelector("#close-modal-1");
-const btnTech1 = document.querySelectorAll("#show-modal-1");
+const btnTech1 = document.querySelectorAll("#button-project-tech-1");
 
 const openTech1 = function () {
     console.log("Button clicked");
-    windowBlog1.classList.remove("hidden");
+    windowTech1.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech1 = function () {
-    windowBlog1.classList.add("hidden");
+    windowTech1.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -32,25 +30,25 @@ overlay.addEventListener("click", closeTech1);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog1.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech1.classList.contains("hidden")) {
         closeTech1();
     }
 });
 
 // Sobat Listrik
 
-const windowBlog2 = document.querySelector("#window-blog-2");
+const windowTech2 = document.querySelector("#window-project-tech-2");
 const btnCloseModal2 = document.querySelector("#close-modal-2");
-const btnTech2 = document.querySelectorAll("#show-modal-2");
+const btnTech2 = document.querySelectorAll("#button-project-tech-2");
 
 const openTech2 = function () {
     console.log("Button clicked");
-    windowBlog2.classList.remove("hidden");
+    windowTech2.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech2 = function () {
-    windowBlog2.classList.add("hidden");
+    windowTech2.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -64,25 +62,25 @@ overlay.addEventListener("click", closeTech2);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog2.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech2.classList.contains("hidden")) {
         closeTech2();
     }
 });
 
 // RPA
 
-const windowBlog3 = document.querySelector("#window-blog-3");
+const windowTech3 = document.querySelector("#window-project-tech-3");
 const btnCloseModal3 = document.querySelector("#close-modal-3");
-const btnTech3 = document.querySelectorAll("#show-modal-3");
+const btnTech3 = document.querySelectorAll("#button-project-tech-3");
 
 const openTech3 = function () {
     console.log("Button clicked");
-    windowBlog3.classList.remove("hidden");
+    windowTech3.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech3 = function () {
-    windowBlog3.classList.add("hidden");
+    windowTech3.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -96,25 +94,25 @@ overlay.addEventListener("click", closeTech3);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog3.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech3.classList.contains("hidden")) {
         closeTech3();
     }
 });
 
 // Community Service
 
-const windowBlog4 = document.querySelector("#window-blog-4");
+const windowTech4 = document.querySelector("#window-project-tech-4");
 const btnCloseModal4 = document.querySelector("#close-modal-4");
-const btnTech4 = document.querySelectorAll("#show-modal-4");
+const btnTech4 = document.querySelectorAll("#button-project-tech-4");
 
 const openTech4 = function () {
     console.log("Button clicked");
-    windowBlog4.classList.remove("hidden");
+    windowTech4.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech4 = function () {
-    windowBlog4.classList.add("hidden");
+    windowTech4.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -128,7 +126,7 @@ overlay.addEventListener("click", closeTech4);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog4.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech4.classList.contains("hidden")) {
         closeTech2();
     }
 });
@@ -137,18 +135,18 @@ document.addEventListener("keydown", function (e) {
 
 // Clicknic
 
-const windowBlog5 = document.querySelector("#window-blog-5");
+const windowTech5 = document.querySelector("#window-project-tech-5");
 const btnCloseModal5 = document.querySelector("#close-modal-5");
-const btnTech5 = document.querySelectorAll("#show-modal-5");
+const btnTech5 = document.querySelectorAll("#button-project-tech-5");
 
 const openTech5 = function () {
     console.log("Button clicked");
-    windowBlog5.classList.remove("hidden");
+    windowTech5.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech5 = function () {
-    windowBlog5.classList.add("hidden");
+    windowTech5.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -162,25 +160,25 @@ overlay.addEventListener("click", closeTech5);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog5.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech5.classList.contains("hidden")) {
         closeTech5();
     }
 });
 
 // Sobat Listrik
 
-const windowBlog6 = document.querySelector("#window-blog-6");
+const windowTech6 = document.querySelector("#window-project-tech-6");
 const btnCloseModal6 = document.querySelector("#close-modal-6");
-const btnTech6 = document.querySelectorAll("#show-modal-6");
+const btnTech6 = document.querySelectorAll("#button-project-tech-6");
 
 const openTech6 = function () {
     console.log("Button clicked");
-    windowBlog6.classList.remove("hidden");
+    windowTech6.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech6 = function () {
-    windowBlog6.classList.add("hidden");
+    windowTech6.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -194,25 +192,25 @@ overlay.addEventListener("click", closeTech6);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog6.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech6.classList.contains("hidden")) {
         closeTech6();
     }
 });
 
 // RPA
 
-const windowBlog7 = document.querySelector("#window-blog-7");
+const windowTech7 = document.querySelector("#window-project-tech-7");
 const btnCloseModal7 = document.querySelector("#close-modal-7");
-const btnTech7 = document.querySelectorAll("#show-modal-7");
+const btnTech7 = document.querySelectorAll("#button-project-tech-7");
 
 const openTech7 = function () {
     console.log("Button clicked");
-    windowBlog7.classList.remove("hidden");
+    windowTech7.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech7 = function () {
-    windowBlog7.classList.add("hidden");
+    windowTech7.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -226,25 +224,25 @@ overlay.addEventListener("click", closeTech7);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog7.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech7.classList.contains("hidden")) {
         closeTech7();
     }
 });
 
 // Community Service
 
-const windowBlog8 = document.querySelector("#window-blog-8");
+const windowTech8 = document.querySelector("#window-project-tech-8");
 const btnCloseModal8 = document.querySelector("#close-modal-8");
-const btnTech8 = document.querySelectorAll("#show-modal-8");
+const btnTech8 = document.querySelectorAll("#button-project-tech-8");
 
 const openTech8 = function () {
     console.log("Button clicked");
-    windowBlog8.classList.remove("hidden");
+    windowTech8.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
 
 const closeTech8 = function () {
-    windowBlog8.classList.add("hidden");
+    windowTech8.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
@@ -258,7 +256,7 @@ overlay.addEventListener("click", closeTech8);
 document.addEventListener("keydown", function (e) {
     console.log(e.key);
 
-    if (e.key === "Escape" && !windowBlog8.classList.contains("hidden")) {
+    if (e.key === "Escape" && !windowTech8.classList.contains("hidden")) {
         closeTech8();
     }
 });
