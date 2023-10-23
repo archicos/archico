c7c3730837deab7e30629dd619d8930ebbc25b51
ifs21011-itdel <archicosemb@gmail.com>
2023-10-23 21:20:02 +0700

    Responsiveness Fix

--- script.js
+++ script.js
@@ -5,35 +5,3 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
-
-// FORM WINDOW
-
-let formButton = document.querySelector("#feedback-button");
-let formCloseButton = document.querySelector(".form-close-button");
-let formWindow = document.querySelector('#form-window');
-
-// const openForm = function () {
-//     formWindow.classList.remove('hidden');
-// };
-
-// const closeForm = function () {
-//     formWindow.classList.add('hidden');
-// };
-
-// for (let i = 0; i < formButton.length; i++) {
-//     formButton[i].addEventListener("click", openForm);
-// }
-
-// formButton.addEventListener("click", openForm);
-// formCloseButton.addEventListener("click", closeForm);
-
-// document.querySelector("#menu-icon").addEventListener("click", function(){
-//     document.querySelector(".form-window").classList.remove("hidden");
-// });
-
-document.querySelector(".form-button").addEventListener("click", function(){
-    document.querySelector("#form-window").classList.remove("hidden");
-});
-document.querySelector(".form-button").addEventListener("click", function(){
-    document.querySelector("#form-window").classList.remove("hidden");
-});
